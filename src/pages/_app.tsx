1894bce2d73a313b76e525c2353fe8663994d33c
huda-digitsense <114890923+huda-digitsense@users.noreply.github.com>
2024-05-01 05:12:05 +0700

    style: integrate antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
+import React from "react";
 
-import "@/styles/globals.css";
+import theme from "../theme/themeConfig";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <ConfigProvider theme={theme}>
+    <Component {...pageProps} />
+  </ConfigProvider>
+);
+
+export default App;
