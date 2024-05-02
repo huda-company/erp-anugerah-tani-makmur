0156b232af23cfe03c07104fa364a29cc6df1622
huda23aduh <huda23aduh@gmail.com>
2024-05-02 19:08:48 +0700

    Revert "style: integrate antd (#2)" (#5)
    This reverts commit 057bcc1e8bcba49c5148a6dbd83235a62f9b9807.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
-import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
-import React from "react";
 
-import theme from "../theme/themeConfig";
+import "@/styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ConfigProvider theme={theme}>
-    <Component {...pageProps} />
-  </ConfigProvider>
-);
-
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
