ba180072b6354209fb365f3d323a7255d9bf2972
huda-digitsense <114890923+huda-digitsense@users.noreply.github.com>
2024-05-13 19:29:58 +0700

    fix: change function name

--- config/serverResponse.ts
+++ config/serverResponse.ts
@@ -9,7 +9,7 @@ export const statCode = {
   500: { status: 500 },
 };
 
-function createResponse<T>(success: boolean, message: string): ApiResponse<T> {
+function cResp<T>(success: boolean, message: string): ApiResponse<T> {
   return {
     success,
     message,
@@ -23,49 +23,43 @@ export const API_MSG = {
 
 export const respBody = {
   ERROR: {
-    UNEXPECTED_ERROR: createResponse(false, ErrorType.UNEXPECTED_ERROR),
-    INVALID_PO: createResponse(false, ErrorType.INVALID_PO),
-    INCORRECT_PAYLOAD: createResponse(false, ErrorType.INCORRECT_PAYLOAD),
-    UPLOAD_ERROR: createResponse(false, ErrorType.UPLOAD_ERROR),
-    EXPIRED_INVALID_TOKEN: createResponse(
-      false,
-      ErrorType.EXPIRED_INVALID_TOKEN
-    ),
-    INC_EMAIL_PASSWORD: createResponse(false, ErrorType.INC_EMAIL_PASSWORD),
-    EMAIL_OR_PHONE_ALREADY_REGISTERED: createResponse(
+    UNEXPECTED_ERROR: cResp(false, ErrorType.UNEXPECTED_ERROR),
+    INVALID_PO: cResp(false, ErrorType.INVALID_PO),
+    INCORRECT_PAYLOAD: cResp(false, ErrorType.INCORRECT_PAYLOAD),
+    UPLOAD_ERROR: cResp(false, ErrorType.UPLOAD_ERROR),
+    EXPIRED_INVALID_TOKEN: cResp(false, ErrorType.EXPIRED_INVALID_TOKEN),
+    INC_EMAIL_PASSWORD: cResp(false, ErrorType.INC_EMAIL_PASSWORD),
+    EMAIL_OR_PHONE_ALREADY_REGISTERED: cResp(
       false,
       ErrorType.EMAIL_OR_PHONE_ALREADY_REGISTERED
     ),
-    UNKNOWN_EMAIL: createResponse(false, ErrorType.UNKNOWN_EMAIL),
-    PURCHASE_UPDATE: createResponse(false, ErrorType.PURCHASE_UPDATE),
+    UNKNOWN_EMAIL: cResp(false, ErrorType.UNKNOWN_EMAIL),
+    PURCHASE_UPDATE: cResp(false, ErrorType.PURCHASE_UPDATE),
   },
   SUCCESS: {
-    RETRIEVED_DATA_SUCCESS: createResponse(
-      true,
-      SuccessType.RETRIEVED_DATA_SUCCESS
-    ),
-    SIGN_IN_SUCCESS: createResponse(true, SuccessType.SIGN_IN_SUCCESS),
-    UPLOAD_FILE_SUCCESS: createResponse(true, SuccessType.UPLOAD_FILE_SUCCESS),
-    NEW_ROLE_CREATE: createResponse(true, SuccessType.NEW_ROLE_CREATE),
-    NEW_SUPPLIER_CREATE: createResponse(true, SuccessType.NEW_SUPPLIER_CREATE),
-    SUPPLIER_UPDATE: createResponse(true, SuccessType.SUPPLIER_UPDATE),
-    SUPPLIER_DELETE: createResponse(true, SuccessType.SUPPLIER_DELETE),
-    NEW_USER_CREATE: createResponse(true, SuccessType.NEW_USER_CREATE),
-    USER_UPDATE: createResponse(true, SuccessType.USER_UPDATE),
-    USER_DELETE: createResponse(true, SuccessType.USER_DELETE),
-    NEW_ITEM_CAT_CREATE: createResponse(true, SuccessType.NEW_ITEM_CAT_CREATE),
-    BRANCH_UPDATE: createResponse(true, SuccessType.BRANCH_UPDATE),
-    BRANCH_DELETE: createResponse(true, SuccessType.BRANCH_DELETE),
-    ITEM_CAT_UPDATE: createResponse(true, SuccessType.ITEM_CAT_UPDATE),
-    ITEM_CAT_DELETE: createResponse(true, SuccessType.ITEM_CAT_DELETE),
-    NEW_ITEM_CREATE: createResponse(true, SuccessType.NEW_ITEM_CREATE),
-    ITEM_UPDATE: createResponse(true, SuccessType.ITEM_UPDATE),
-    ITEM_DELETE: createResponse(true, SuccessType.ITEM_DELETE),
-    NEW_PURCHASE_CREATE: createResponse(true, SuccessType.NEW_PURCHASE_CREATE),
-    PURCHASE_UPDATE: createResponse(true, SuccessType.PURCHASE_UPDATE),
-    PURCHASE_DELETE: createResponse(true, SuccessType.PURCHASE_DELETE),
-    NEW_UNIT_CREATE: createResponse(true, SuccessType.NEW_UNIT_CREATE),
-    UNIT_UPDATE: createResponse(true, SuccessType.UNIT_UPDATE),
-    UNIT_DELETE: createResponse(true, SuccessType.UNIT_DELETE),
+    RETRIEVED_DATA_SUCCESS: cResp(true, SuccessType.RETRIEVED_DATA_SUCCESS),
+    SIGN_IN_SUCCESS: cResp(true, SuccessType.SIGN_IN_SUCCESS),
+    UPLOAD_FILE_SUCCESS: cResp(true, SuccessType.UPLOAD_FILE_SUCCESS),
+    NEW_ROLE_CREATE: cResp(true, SuccessType.NEW_ROLE_CREATE),
+    NEW_SUPPLIER_CREATE: cResp(true, SuccessType.NEW_SUPPLIER_CREATE),
+    SUPPLIER_UPDATE: cResp(true, SuccessType.SUPPLIER_UPDATE),
+    SUPPLIER_DELETE: cResp(true, SuccessType.SUPPLIER_DELETE),
+    NEW_USER_CREATE: cResp(true, SuccessType.NEW_USER_CREATE),
+    USER_UPDATE: cResp(true, SuccessType.USER_UPDATE),
+    USER_DELETE: cResp(true, SuccessType.USER_DELETE),
+    NEW_ITEM_CAT_CREATE: cResp(true, SuccessType.NEW_ITEM_CAT_CREATE),
+    BRANCH_UPDATE: cResp(true, SuccessType.BRANCH_UPDATE),
+    BRANCH_DELETE: cResp(true, SuccessType.BRANCH_DELETE),
+    ITEM_CAT_UPDATE: cResp(true, SuccessType.ITEM_CAT_UPDATE),
+    ITEM_CAT_DELETE: cResp(true, SuccessType.ITEM_CAT_DELETE),
+    NEW_ITEM_CREATE: cResp(true, SuccessType.NEW_ITEM_CREATE),
+    ITEM_UPDATE: cResp(true, SuccessType.ITEM_UPDATE),
+    ITEM_DELETE: cResp(true, SuccessType.ITEM_DELETE),
+    NEW_PURCHASE_CREATE: cResp(true, SuccessType.NEW_PURCHASE_CREATE),
+    PURCHASE_UPDATE: cResp(true, SuccessType.PURCHASE_UPDATE),
+    PURCHASE_DELETE: cResp(true, SuccessType.PURCHASE_DELETE),
+    NEW_UNIT_CREATE: cResp(true, SuccessType.NEW_UNIT_CREATE),
+    UNIT_UPDATE: cResp(true, SuccessType.UNIT_UPDATE),
+    UNIT_DELETE: cResp(true, SuccessType.UNIT_DELETE),
   },
 };
