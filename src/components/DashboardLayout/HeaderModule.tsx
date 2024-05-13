1efe526a84eda15fc074bb2d0d202e428d44787c
huda23aduh <huda23aduh@gmail.com>
2024-05-13 17:23:54 +0700

    feat: tooltip on header module (#41)
    Co-authored-by: huda-digitsense <114890923+huda-digitsense@users.noreply.github.com>

--- src/components/DashboardLayout/HeaderModule.tsx
+++ src/components/DashboardLayout/HeaderModule.tsx
@@ -9,6 +9,8 @@ import { BsArrowLeftSquare } from "react-icons/bs";
 import { noop } from "^/utils/helpers";
 import { capitalizeStr } from "^/utils/capitalizeStr";
 import { MdOutlineAddToPhotos } from "react-icons/md";
+import CustomToolTip from "../CustomTooltip";
+import { useTranslations } from "next-intl";
 
 const CustomBcumbNoSSR = dynamic(
   () => import("../CustomBreadcrumb/CustomBreadcrumb"),
@@ -17,23 +19,33 @@ const CustomBcumbNoSSR = dynamic(
 
 const HeaderModule: FC<HeaderModuleProps> = ({ title, bcumbs, addPageURL }) => {
   const router = useRouter();
+  const t = useTranslations("");
 
   return (
     <div className="flex justify-between rounded-lg border-2 border-primary p-1 dark:border-gray-700 ">
       <div className="flex gap-x-3">
         <div className="flex flex-row items-center gap-x-3">
           <div onClick={router.back} className="cursor-pointer">
-            <BsArrowLeftSquare color="black" size={20} />
+            <CustomToolTip
+              elm={<BsArrowLeftSquare color="black" size={20} />}
+              content={<p>{capitalizeStr(t("Common.back"))}</p>}
+            />
           </div>
+
           <Typography className="text-xl font-bold text-black underline">
             {capitalizeStr(title)}
           </Typography>
 
-          <MdOutlineAddToPhotos
-            onClick={() => {
-              addPageURL ? router.push(addPageURL) : noop;
-            }}
-            className="h-6 w-6 cursor-pointer"
+          <CustomToolTip
+            elm={
+              <MdOutlineAddToPhotos
+                onClick={() => {
+                  addPageURL ? router.push(addPageURL) : noop;
+                }}
+                className="h-6 w-6 cursor-pointer"
+              />
+            }
+            content={<p>{capitalizeStr(t("Common.add"))}</p>}
           />
         </div>
       </div>
