0a7de48ec5400a752acc0b434d806844fae0ec6d
huda-digitsense <114890923+huda-digitsense@users.noreply.github.com>
2024-06-20 06:14:40 +0700

    fix: remove comment

--- services/supplier-stock.ts
+++ services/supplier-stock.ts
@@ -1,4 +1,4 @@
-import { ISupplierFieldRequest, ISupplierForm } from "^/@types/models/supplier";
+import { ISupplierFieldRequest } from "^/@types/models/supplier";
 import { API_VERSION, BASE_URL } from "^/config/env";
 import { buildReqHeader } from "^/config/service";
 import { objToQueryURL } from "^/utils/helpers";
@@ -20,54 +20,6 @@ export const getSupplierStockAPI = async (
 
   try {
     return await axios.create(reqHeader).get(reqURL);
-  } catch (error: any) {
-    return error;
-  }
-};
-
-export const addSupplierAPI = async (
-  sess: Session | null,
-  params: ISupplierForm
-) => {
-  if (!sess) return null;
-
-  const reqURL = `${BASE_SUPPLIER_STOCK_API_URL}`;
-
-  const reqHeader = buildReqHeader(String(sess.accessToken));
-
-  try {
-    return await axios.create(reqHeader).post(reqURL, params);
-  } catch (error: any) {
-    return error;
-  }
-};
-
-export const editSupplierAPI = async (
-  sess: Session | null,
-  params: ISupplierForm
-) => {
-  if (!sess) return null;
-
-  const reqURL = `${BASE_SUPPLIER_STOCK_API_URL}/update/${params.id}`;
-
-  const reqHeader = buildReqHeader(String(sess.accessToken));
-
-  try {
-    return await axios.create(reqHeader).patch(reqURL, params);
-  } catch (error: any) {
-    return error;
-  }
-};
-
-export const deleteSupplierAPI = async (sess: Session | null, id: string) => {
-  if (!sess) return null;
-
-  const reqURL = `${BASE_SUPPLIER_STOCK_API_URL}/delete/${id}`;
-
-  const reqHeader = buildReqHeader(String(sess.accessToken));
-
-  try {
-    return await axios.create(reqHeader).delete(reqURL);
   } catch (error: any) {
     return error;
   }
