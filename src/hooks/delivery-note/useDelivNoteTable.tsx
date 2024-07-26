3a98881cfd8d0444fa09f2ce97cc311f15a3a578
huda-digitsense <114890923+huda-digitsense@users.noreply.github.com>
2024-07-27 06:52:48 +0700

    fix: integrate rct query n rct table on delivnote  module

--- src/hooks/delivery-note/useDelivNoteTable.tsx
+++ src/hooks/delivery-note/useDelivNoteTable.tsx
@@ -5,83 +5,158 @@ import {
   getSortedRowModel,
 } from "@tanstack/react-table";
 import { useState } from "react";
-import useDebounce from "@/hooks/useDebounce";
-import { PaginationCustomPrms } from "@/components/PaginationCustom/types";
 import { initSuppReqPrm } from "^/config/supplier/config";
 import useGetDelivNote from "./useGetDelivNote";
-import { DelivNoteTanTblData } from "^/@types/models/deliverynote";
+import { IDelivNoteGetReq } from "^/@types/models/deliverynote";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
 
-const useDelivNoteTable = (
-  data: DelivNoteTanTblData[],
-  columns: any,
-  itemPgntn: PaginationCustomPrms
-) => {
-  const { reqPrm, setReqPrm } = useGetDelivNote();
+const useDelivNoteTable = (columns: any) => {
+  const { data: session } = useSession();
 
+  const queryClient = useQueryClient();
+  const delivNotePgntn = queryClient.getQueryData<IDelivNoteGetReq>([
+    "delivNotePgntn",
+  ]);
+  const glbFltr = queryClient.getQueryData<any>(["delivNoteGlobFltr"]);
+  const delivNoteData = queryClient.getQueryData<any>(["delivNote"]);
+
+  const { fetchData } = useGetDelivNote();
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [globalFilter, setGlobalFilter] = useState("");
-  const debGlobFltr = useDebounce(globalFilter, 500); // Adjust delay as needed
+
+  const pgntMut = useMutation({
+    mutationFn: async (prm: IDelivNoteGetReq) => {
+      await queryClient.setQueryData(
+        ["delivNoteGlobFltr"],
+        prm["param[search]"]
+      );
+
+      return await fetchData(session, prm);
+    },
+    onSettled: async (mutRes: any) => {
+      await queryClient.setQueryData(["delivNotePgntn"], {
+        ...delivNotePgntn,
+        page: 1,
+        limit: mutRes.limit ?? 1,
+      });
+    },
+  });
+
+  const handleGlobFltrChange = (prm: string) => {
+    if (delivNotePgntn) {
+      pgntMut.mutate({
+        ...delivNotePgntn,
+        "param[search]": prm,
+      });
+    }
+  };
 
   const table = useReactTable({
-    data,
+    data: delivNoteData ? delivNoteData?.items : [],
     columns,
     getCoreRowModel: getCoreRowModel(),
     manualFiltering: true,
     state: {
       columnFilters,
-      globalFilter,
+      globalFilter: glbFltr,
       pagination: {
-        pageIndex: Number(itemPgntn.page - 1),
-        pageSize: itemPgntn.limit,
+        pageIndex: Number(delivNoteData?.page - 1),
+        pageSize: delivNoteData?.limit,
       },
     },
     getSortedRowModel: getSortedRowModel(),
     onColumnFiltersChange: setColumnFilters,
-    onGlobalFilterChange: setGlobalFilter,
+    onGlobalFilterChange: handleGlobFltrChange,
     enableFilters: true,
     enableColumnFilters: true,
     manualPagination: true, //turn off client-side pagination
-    pageCount: itemPgntn.totalPages, //pass in the total row count so the table knows how many pages there are (pageCount calculated internally if not provided)
+    pageCount: delivNoteData?.totalPages, //pass in the total row count so the table knows how many pages there are (pageCount calculated internally if not provided)
     debugTable: true,
     debugHeaders: false,
     debugColumns: false,
-    debugRows: false,
+    debugRows: true,
   });
-
-  const handleNextPgnt = () => {
-    table.setPagination({
-      pageIndex: table.getState().pagination.pageIndex + 1,
-      pageSize: table.getState().pagination.pageSize,
-    });
+  const handleResetFilter = async () => {
+    if (delivNotePgntn) {
+      pgntMut.mutate(initSuppReqPrm);
+    }
   };
 
-  const handlePrevPgnt = () => {
-    table.setPagination({
-      pageIndex: table.getState().pagination.pageIndex - 1,
-      pageSize: table.getState().pagination.pageSize,
-    });
+  const handleFirstPageClck = () => {
+    if (delivNotePgntn) {
+      pgntMut.mutate({
+        ...delivNotePgntn,
+        page: 0,
+        limit: delivNotePgntn.limit,
+      });
+    }
   };
 
-  const handleResetFilter = () => {
-    setGlobalFilter("");
-    setReqPrm({
-      ...initSuppReqPrm,
-      "param[search]": "",
-      page: 1,
-      limit: reqPrm.limit,
-    });
+  const handleLastPageClck = () => {
+    const lastPage = table.getPageCount();
+    if (delivNotePgntn) {
+      pgntMut.mutate({
+        ...delivNotePgntn,
+        page: lastPage,
+        limit: delivNotePgntn.limit,
+      });
+    }
   };
 
+  const handleNextClck = () => {
+    const newPage = Number(delivNoteData.page) + 1;
+    if (delivNotePgntn) {
+      pgntMut.mutate({
+        ...delivNotePgntn,
+        page: newPage,
+        limit: delivNotePgntn.limit,
+      });
+    }
+  };
+
+  const handlePrevClck = () => {
+    const newPage = Number(delivNoteData.page) - 1;
+    if (delivNotePgntn) {
+      pgntMut.mutate({
+        ...delivNotePgntn,
+        page: newPage,
+        limit: delivNotePgntn.limit,
+      });
+    }
+  };
+
+  const handlePageInputChange = (prm: number) => {
+    if (delivNotePgntn) {
+      queryClient.setQueryData(["delivNotePgntn"], {
+        ...delivNotePgntn,
+        page: Number(prm),
+      });
+      fetchData(session, { ...delivNotePgntn, page: Number(prm) });
+    }
+  };
+
+  const handlePageRowChange = async (prm: number) => {
+    if (delivNotePgntn) {
+      await pgntMut.mutate({
+        ...delivNotePgntn,
+        page: 1,
+        limit: prm ?? 1,
+      });
+    }
+  };
   return {
     table,
     columnFilters,
-    globalFilter,
-    debGlobFltr,
+    pgntMut,
     setColumnFilters,
-    setGlobalFilter,
-    handleNextPgnt,
-    handlePrevPgnt,
+    handleGlobFltrChange,
     handleResetFilter,
+    handleFirstPageClck,
+    handleLastPageClck,
+    handleNextClck,
+    handlePrevClck,
+    handlePageRowChange,
+    handlePageInputChange,
   };
 };
 
